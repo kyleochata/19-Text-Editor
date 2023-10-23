40a12f04361176f8742db42b11483bb343b7daa9
kyleochata <kyleochata@gmail.com>
2023-10-22 17:18:29 -0700

    typo in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('src', 'icons')
+            destination: path.join('assets', 'icons')
           }
         ]
       })
